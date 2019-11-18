5702fcb7eca39cffe4ff544056833e51afd60710
Hugo Retief <hugoretief@gmail.com>
2019-11-18 11:09:26 +0200

    Adding code book and final run_analysis.R script

--- run_analysis.R
+++ run_analysis.R
@@ -11,11 +11,9 @@ dir <- "UCI HAR Dataset"
 
 #Bind activity and subject columns to the dataset
 add_sub_act <- function(group,ds){
-  print(group)
-  act_desc <- read.table(paste0(dir,"//activity_labels.txt"), header = FALSE, sep = "")
-  #print(str(act_desc))
   
   #Add activity column
+  act_desc <- read.table(paste0(dir,"//activity_labels.txt"), header = FALSE, sep = "")
   act_column <- read.table(paste0(dir,"//",group,"//y_",group,".txt"), header = FALSE, sep = "")
   print(str(act_column))
   merge_column <- merge(act_desc, act_column, all.x=TRUE)
@@ -25,7 +23,6 @@ add_sub_act <- function(group,ds){
   #Add subject column
   sub_column <- read.table(paste0(dir,"//",group,"//subject_",group,".txt"), header = FALSE, sep = "")
   ds <- cbind("Subject" = sub_column$V1, ds)
-  
   ds <- ds[!duplicated(as.list(ds))]
   ds_final <- select(ds,  one_of ("Subject", "Activity"), matches("mean|std"))  
   ds_final
@@ -35,7 +32,7 @@ add_sub_act <- function(group,ds){
 
 #Restructure coulmn names to make them more readable then assign them to the dataset in question
 assign_col_names <- function(group){
-  print(group)
+
   #Restructure the coulmn names
   features <- read.table(paste0(dir,"//features.txt"), header = FALSE, sep = "")
   columnNames = features$V2
@@ -45,6 +42,8 @@ assign_col_names <- function(group){
   #Assign the restructered headers to the dataset
   ds = read.table(paste0(dir,"//",group,"//x_",group,".txt"), header=FALSE)
   names(ds) <- c(columnNames)
+  
+  #excute the function that adds the subjects and activities to the dataset
   add_sub_act(group, ds)
   }
 
@@ -60,14 +59,15 @@ if (!file.exists(dir)) {
 }
 
 
+#Start the execution of data processing the train and test datasets
 test_data <- assign_col_names("test")
-
 train_data <- assign_col_names("train")
 
-#Merges the training and the test sets to create one data set.
+
+#Merges the training and the test sets to create one data set and outputs it as a file called merged_tidy_data.csv.
 
 merged_dataset <- rbind(test_data,train_data)
-fwrite(merged_dataset, "tidy_data.txt")
+fwrite(merged_dataset, "merged_tidy_data.csv")
 
-
-
+#Outputs the mean values for each variable for each activity carried out by each Subject and outputs the result to a file called mean_tidy_data.csv
+fwrite(merged_dataset %>% group_by(.dots=c("Subject","Activity")) %>% summarise_each(funs(mean)), "mean_tidy_data.csv")
